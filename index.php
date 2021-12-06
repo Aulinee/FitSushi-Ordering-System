1fbdbbf643ae3ce639957b7f4c6d7a582e64372e
Aulinee <audreyduyan1@gmail.com>
2021-12-07 02:24:45 +0800

    add landing page

--- index.php
+++ index.php
@@ -4,12 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poller+One&display=swap" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/nats" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link rel="stylesheet" href="style/style.css">
+    <title>Landing Page</title>
 </head>
 <body>
-    <p> nadai temu utai </p>
-    <p> udah temu utai </p>
-    <p>udah nemu</p>
-    <p> happy 21st birthday Venessa</p>
+    <header id="navbar" class="">
+        <div class="container">
+            <img class="main-logo left" src="img/logo.png" alt="logo" width="80px">
+            <ul class="right">
+                <li><a class="home-tab" href="#">Home</a></li>
+                <li><button class="sign-in-btn">Sign In</button></li>
+            </ul>
+        </div>
+    </header>
+    <section class="landing-content">
+        <div class="landing-content-left">
+            <div class="title-1">
+                <h1>Have you get your craving sushified?</h1>
+            </div>
+            <div class="title-2">
+                <h1>Don’t worry. We deliver fresh sushi wherever you are.</h1>
+            </div>
+            <div class="">
+                <button class="order-btn">Order Now</button>
+            </div>
+        </div>
+        <div class="landing-content-right">
+            <img class="sushi-logo right" src="img/small landing page 1.png" alt="logo">
+        </div>
+    </section>
+    <footer class="footer">
+        <h1>&copy; Copyright 2021 FitSushi</h1>
+    </footer>
 </body>
 </html>
