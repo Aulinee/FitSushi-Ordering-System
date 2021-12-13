0348858d02abcdaaec53dff63d3b32f1c09ee916
Vee6 <95630539+Vee6@users.noreply.github.com>
2021-12-13 15:48:11 +0800

    Admin test

---
+++ Admin-Folder/Dashboard.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poller+One&display=swap" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/nats" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Neucha" />
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/admin.css">
+    <title>Home</title>
+</head>
+<body>
+    <section class="admin-page">
+            <div class="admin-page-sidebar">
+                <div>
+                    <h1><img src="img\title.jpeg" alt="FitSushi logo"></h1>
+                </div>
+                <div>
+                    <h1><img src="img\admin\1024px-Circle-icons-profile.svg.png" alt="Admin picture"> Susan <br> Admin </h1>
+                </div>
+            </div>
+            <div class="admin-page-dashboard">
+
+            </div>
+    </section>
+
+
+    <footer class="footer">
+        <h1>&copy; Copyright 2021 FitSushi</h1>
+    </footer>
+</body>
