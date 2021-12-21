c512aec5097f4bc4a0d43636bcce99a6c3342d22
Aulinee <audreyduyan1@gmail.com>
2021-12-21 14:55:03 +0800

    update menu class

--- class/MenuClass.php
+++ class/MenuClass.php
@@ -1,18 +1,5 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "fitsushi";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}else{
-    
-}
+include '../database/dbConnection.php'; 
 
 class Menu{
     /* Constructor */
